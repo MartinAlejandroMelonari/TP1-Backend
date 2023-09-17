8efec573968ad3bd0ffdfdb2a876bea8ee449f12
MartinAlejandroMelonari <144966823+MartinAlejandroMelonari@users.noreply.github.com>
2023-09-17 03:12:13 -0300

    Delete TP1 directory

--- TP1/src/main/java/com/example/TP1/repositorios/RubrosRep.java
+++
@@ -1,9 +0,0 @@
-package com.example.TP1.repositorios;
-
-import com.example.TP1.entidades.Rubro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RubrosRep extends JpaRepository<Rubro,Long> {
-}
